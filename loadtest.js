5ae20990b4ae6e4e22a4c4912191e8058e9b53c2
Lucas Liu <lucas.liu@duke.edu>
2021-04-28 21:23:21 -0400

    initial k6 loadtest

---
+++ loadtest.js
@@ -0,0 +1,20 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+const url = "http://20.81.8.9/predict";
+const data = require('./employees.json');
+
+export const options = {
+  duration: '1m',
+  vus: 10,
+  thresholds: {
+    http_req_duration: ['p(95)<2500'],
+  },
+};
+
+export default function () {
+    let res = http.post(url, JSON.stringify(data),
+                       { headers: { 'Content-Type': 'application/json' } });
+    console.log(res);
+  sleep(1);
+}
